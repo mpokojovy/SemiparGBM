86257cedf64aa00886c6f640ab97a52e083fc6b7
Michael Pokojovy, Ph.D <64013730+mpokojovy@users.noreply.github.com>
2024-05-27 07:30:51 -0400

    Add files via upload

--- assessment/regul.stats.R
+++ assessment/regul.stats.R
@@ -1,12 +1,12 @@
 # (C) Andrews T. Anum (2024)
 
-regul.cov.mat <- function(S){
+regul.cov.mat <- function(S, max.cond.k = 50){
   
   d <- ncol(S)
-  max.cond.k <- 50
+  #max.cond.k <- 50
   
   #T <- diag(d)
-  T <- target.matrix(S)
+  T <- target.matrix(S, max.cond.k)
   
   #regularize Target matrix
   decomp.T <- svd(T)
@@ -18,8 +18,19 @@ regul.cov.mat <- function(S){
   Su.matrix <- diag(D.n.onehalf)%*%Q.t%*%S%*%Q%*%diag(D.n.onehalf)
   eigen.vals <- eigen(Su.matrix)$values
   pos.eigen.vals <- abs(eigen.vals)
+  max.eigen <- max(pos.eigen.vals)
+  min.eigen <- min(pos.eigen.vals)
   
-  rho.k <- (-max(pos.eigen.vals) + 50*min(abs(eigen.vals)))/( -49 + (-max(pos.eigen.vals)+  50*min(pos.eigen.vals)) )
+  gridpoints = seq(from = 0, to = 1, length.out = 200)
+  
+  for (i in 1:length(gridpoints)) {
+    con.num.k = (gridpoints[i] + (1-gridpoints[i])*max.eigen) / (gridpoints[i] + (1-gridpoints[i])*min.eigen)
+    if(con.num.k <= max.cond.k){
+      rho.k = gridpoints[i]
+      #print(rho.k)
+      break
+    }
+  }
   
   regmat <- (rho.k*T + (1 - rho.k)*S)
   
@@ -28,15 +39,15 @@ regul.cov.mat <- function(S){
 }
 
 
-target.matrix <- function(xdata){
+target.matrix <- function(xdata, max.cond.k){
   
   robscale <- apply(xdata, 2, robustbase::Qn)
   #robscale <- sqrt(robscale)
   tar.mat <- diag(robscale)
   cond.num.T <- kappa(tar.mat, exact = TRUE) 
   
-  if(cond.num.T > 50){
-    threshd <- max(abs(robscale))/45
+  if(cond.num.T > max.cond.k){
+    threshd <- max(abs(robscale))/(0.9*max.cond.k)
     robscale.update <- ifelse(robscale < threshd, threshd, robscale)
     tar.mat <- diag(robscale.update)
   }
